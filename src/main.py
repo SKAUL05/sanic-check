b16ee6a5efc82566a613ea001377933b209c606b
Sarath Kaul <kaul.sarath@gmail.com>
2020-05-27 06:39:22 +0000

    Logger Added

--- src/main.py
+++ src/main.py
@@ -1,11 +1,13 @@
 from sanic import Sanic
-from sanic.response import json
+from sanic.response import json, text
+from sanic.log import logger
 
 app = Sanic("hello_example")
 
 @app.route("/test")
 async def test(request):
-  return json({"hello": "world"})
+    logger.info("Initialized")
+    return json({"hello": "world"})
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", port=8000)
