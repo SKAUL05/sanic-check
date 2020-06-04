31cb00214d45d8ef1a8f53c7fbb8e3dfefab8c0b
Sarath Kaul <kaul.sarath@gmail.com>
2020-06-04 07:33:10 +0000

    Render Template Function Added

--- src/main.py
+++ src/main.py
@@ -2,11 +2,21 @@ from sanic import Sanic
 from sanic.response import json, text, html
 from sanic.log import logger
 
-from jinja2 import Environment, PackageLoader
+from jinja2 import Environment, PackageLoader, select_autoescape
 
 
 app = Sanic("hello_example")
-env = Environment(loader=PackageLoader("main",'templates'))
+env = Environment(loader=PackageLoader("main", "templates"))
+
+env = Environment(
+    loader=PackageLoader("main", "templates"),
+    autoescape=select_autoescape(["html", "xml", "tpl"]),
+)
+
+
+def template(tpl, **kwargs):
+    template = env.get_template(tpl)
+    return html(template.render(kwargs))
 
 
 @app.route("/test")
@@ -14,12 +24,13 @@ async def test(request):
     logger.info("Initialized")
     return json({"hello": "world"})
 
+
 @app.route("/")
 async def default(request):
     logger.info("Template Render")
-    template = env.get_template('index.html')
-    html_content = template.render()
-    return html(html_content)
+
+    return template("index.html", title="Sanic")
+
 
 if __name__ == "__main__":
-  app.run(host="0.0.0.0", debug = True, port=8000)
+    app.run(host="0.0.0.0", debug=True, port=8000)
