6ff4a62eca8223c583e0ab8b565f20e9e769c70c
Sarath Kaul <kaul.sarath@gmail.com>
2020-06-12 08:52:01 +0000

    I Check Added

--- src/main.py
+++ src/main.py
@@ -1,13 +1,17 @@
 from sanic import Sanic
 from sanic.response import json, text
 from sanic.log import logger
+from requests import get
+
+
 
 app = Sanic("hello_example")
 
-@app.route("/test")
+@app.route("/")
 async def test(request):
     logger.info("Initialized")
-    return json({"hello": "world"})
+    ip = get('https://api.ipify.org').text
+    return json({"IP": ip})
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", port=8000)
