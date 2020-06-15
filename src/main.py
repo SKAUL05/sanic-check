c9ba590e0948269cb920b9a7c11c9a558c1c08b5
Sarath Kaul <kaul.sarath@gmail.com>
2020-06-15 16:34:43 +0000

    Data FIlteration

--- src/main.py
+++ src/main.py
@@ -4,16 +4,24 @@ from sanic.log import logger
 from requests import get
 import json as js
 
-
-
 app = Sanic("hello_example")
+
+def filter_country_data(cy_data):
+    search_data = dict()
+    for country in cy_data:
+        search_data[country['alpha3Code']] = country
+    logger.info(len(search_data))
+    return search_data
+
+
 
 @app.route("/countries")
 async def countries(request):
     logger.info("Countries")
     cy_data = js.loads(get("https://restcountries.eu/rest/v2/all").text)
-    logger.info(cy_data)
-    return json(cy_data)
+    logger.info(len(cy_data))
+    search_data = filter_country_data(cy_data)
+    return json(search_data)
     
 
 @app.route("/")
