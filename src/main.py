b169f4327f6ad7480867aaf7de45dbbb38a858e0
Sarath Kaul <kaul.sarath@gmail.com>
2020-06-15 16:16:19 +0000

    Countries REST API Added

--- src/main.py
+++ src/main.py
@@ -2,10 +2,19 @@ from sanic import Sanic
 from sanic.response import json, text
 from sanic.log import logger
 from requests import get
+import json as js
 
 
 
 app = Sanic("hello_example")
+
+@app.route("/countries")
+async def countries(request):
+    logger.info("Countries")
+    cy_data = js.loads(get("https://restcountries.eu/rest/v2/all").text)
+    logger.info(cy_data)
+    return json(cy_data)
+    
 
 @app.route("/")
 async def test(request):
